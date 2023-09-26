cbd0c9eafbf8d6cd57ebfd1ac44818789b6f46ed
Kristiyan Ivanov <kris.ivanov97@gmail.com>
2023-09-26 20:40:01 +0300

    Update rock_paper_scissors.py

--- Rock_Paper_Scissors/rock_paper_scissors.py
+++ Rock_Paper_Scissors/rock_paper_scissors.py
@@ -1,10 +1,13 @@
 import random
+
+from colorama import Fore
+
 moves = ["rock", "paper", "scissors"]
 player_counter = 0
 computer_counter = 0
 
 while True:
-    player_move = input("Choose [r]ock, [p]aper or [s]cissors: ").lower()
+    player_move = input(Fore.WHITE + "Choose [r]ock, [p]aper or [s]cissors: ").lower()
 
     if player_move == "r":
         player_choice = moves[0]
@@ -20,27 +23,28 @@ while True:
     print(F"You chose : {player_choice.upper()} \nComputer chose : {computer_choice.upper()}")
 
     if player_choice == computer_choice:
-        print(F"You both chose : {player_choice.upper()} \nIt's a tie!")
+        print(f"You both chose : {player_choice.upper()}")
+        print(Fore.BLUE + "It's a tie!")
     elif player_choice == "scissors":
         if computer_choice == "paper":
-            print("Scissors cuts paper! You win!")
+            print(Fore.GREEN + "Scissors cuts paper! You win!")
             player_counter += 1
         else:
-            print("Rock smashes scissors! You loose!")
+            print(Fore.RED + "Rock smashes scissors! You loose!")
             computer_counter += 1
     elif player_choice == "paper":
         if computer_choice == "rock":
-            print("Paper covers rock! You win!")
+            print(Fore.GREEN + "Paper covers rock! You win!")
             player_counter += 1
         else:
-            print("Scissors cuts paper! You loose!")
+            print(Fore.RED + "Scissors cuts paper! You loose!")
             computer_counter += 1
     elif player_choice == "rock":
         if computer_choice == "scissors":
-            print("Rock crushes scissors! You win!")
+            print(Fore.GREEN + "Rock crushes scissors! You win!")
             player_counter += 1
         else:
-            print("Paper covers rock! You loose!")
+            print(Fore.RED + "Paper covers rock! You loose!")
             computer_counter += 1
 
     if player_counter == 3:
